9edb1d82dac2875c57ee437cb1bc5922cc062f74
enver ellialti <enverellialti@gmail.com>
2023-05-01 21:05:42 +0300

    görev tamamlandi

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 /* Aşağıda global olarak tanımlanmış değişkenler bulunmaktadır, bunları değiştirmeyin. Açıklamaları takip ederek görevleri tamamlayın. */
 var cumleler = [
-	["Annem","ekmek","almak","için","gitti."],
-	["Babam","her","zaman","çok","sevdi."],
-	["Annemin","bugün","2'de","doğum","günü."],
-	["Olanları","unutup","bir","daha","dönemem."],
-	["Her","zaman","içinde","yarası","var."],
-	["O","zaman","senin","bu","halindim."],
-	["Artık","içimden","bir","şey","gelmiyor."],
-	["Kader","bizi","her","zaman","ayırdı."],
-	["Hiç","bir","zaman","yazılanı","silemedim."],
-	["İçimden","gel","dedim","ama","gelmedin."],
-	["Kader","o","defteri","tekrar","açamaz."],
-	["Şarkılar","seni","bana","hatırlatan","gerçekti."],
-	["Bu","gece","çok","uykum","vardı."],
-	["Neden","böyle","yaptığına","anlam","veremedim."],
-	["Benim","boyum","ondan","bayağı","kısaydı."],
-	["Senin","gibi","birini","sevdim."],
-	["Kader","seni","yeniden","yazamıyorum."],
-	["Neden","bu","kadar","acı?"],
-	["Ben","böyle","olmasını","istemedim."],
-	["Sonumu","ve","önümü","bilemiyorum."],
-	["Sana","gel","bile","diyemiyorum."],
-	["Onu","çok","seviyor."],
-	["Olan","oldu","artık."],
-	["Basım","çok","ağrıyor."],
-	["Neden","böyle","olduk?"],
-	["Nereden","nereye","gelmiştik."],
-	["Düşünmekten,","kendimden","yoruldum."],
-	["Böyle","yaptı."],["Suçlu","değilsin."],
-	["Neden","yaptın?"],
-	["Sana","değer."],
-	["Acım","içimde."],
-	["Annen","hazırdı."],
+	["Annem", "ekmek", "almak", "için", "gitti."],
+	["Babam", "her", "zaman", "çok", "sevdi."],
+	["Annemin", "bugün", "2'de", "doğum", "günü."],
+	["Olanları", "unutup", "bir", "daha", "dönemem."],
+	["Her", "zaman", "içinde", "yarası", "var."],
+	["O", "zaman", "senin", "bu", "halindim."],
+	["Artık", "içimden", "bir", "şey", "gelmiyor."],
+	["Kader", "bizi", "her", "zaman", "ayırdı."],
+	["Hiç", "bir", "zaman", "yazılanı", "silemedim."],
+	["İçimden", "gel", "dedim", "ama", "gelmedin."],
+	["Kader", "o", "defteri", "tekrar", "açamaz."],
+	["Şarkılar", "seni", "bana", "hatırlatan", "gerçekti."],
+	["Bu", "gece", "çok", "uykum", "vardı."],
+	["Neden", "böyle", "yaptığına", "anlam", "veremedim."],
+	["Benim", "boyum", "ondan", "bayağı", "kısaydı."],
+	["Senin", "gibi", "birini", "sevdim."],
+	["Kader", "seni", "yeniden", "yazamıyorum."],
+	["Neden", "bu", "kadar", "acı?"],
+	["Ben", "böyle", "olmasını", "istemedim."],
+	["Sonumu", "ve", "önümü", "bilemiyorum."],
+	["Sana", "gel", "bile", "diyemiyorum."],
+	["Onu", "çok", "seviyor."],
+	["Olan", "oldu", "artık."],
+	["Basım", "çok", "ağrıyor."],
+	["Neden", "böyle", "olduk?"],
+	["Nereden", "nereye", "gelmiştik."],
+	["Düşünmekten,", "kendimden", "yoruldum."],
+	["Böyle", "yaptı."], ["Suçlu", "değilsin."],
+	["Neden", "yaptın?"],
+	["Sana", "değer."],
+	["Acım", "içimde."],
+	["Annen", "hazırdı."],
 	["Bıktım."],
 	["İstedim."],
 	["Yaptım."],
 	["Nedendi?"],
 	["Yoruldum."]
-	];
-	
-	var sebzeler = ['🍅', '🍄', '🥦', '🥒', '🌽', '🥕', '🥑'];
-	var meyveler = ['🍇','🍈','🍉','🍊','🍋','🍌','🍍','🥭','🍎','🍐','🍑','🍒','🍓','🥝','🥥']
-	
-	var emojiler = { 
+];
+
+var sebzeler = ['🍅', '🍄', '🥦', '🥒', '🌽', '🥕', '🥑'];
+var meyveler = ['🍇', '🍈', '🍉', '🍊', '🍋', '🍌', '🍍', '🥭', '🍎', '🍐', '🍑', '🍒', '🍓', '🥝', '🥥']
+
+var emojiler = {
 	":)": "🙂",
 	":(": "😔",
 	":d": "😁",
 	":p": "😛",
 	":o": "😱",
 	"<3": "❤️",
-	}
+}
 
 
 
@@ -69,25 +69,32 @@ var cumleler = [
 	*/
 
 // ÖRNEĞİN ÇÖZÜMÜ:
-function cumleKur(birinci, ikinci="", ucuncu="", dorduncu="", besinci=""){
-	return birinci+ikinci+ucuncu+dorduncu+besinci;
+function cumleKur(birinci, ikinci = "", ucuncu = "", dorduncu = "", besinci = "") {
+	return birinci + ikinci + ucuncu + dorduncu + besinci;
 }
 
 /* (Oto test yok) cumleKur fonksiyonuna yalnızca 1 parametre göndererek "Hello World!" stringini elde edin, sonucu konsolde gözlemleyin */
 
 
-
-
+function cumleKur1(cumle) {
+	console.log(cumle);
+}
+cumleKur1("Hello World!");
 /* (Oto test yok) cumleKur fonksiyonuna yalnızca 2 parametre göndererek "Hello World!" stringini elde edin, sonucu konsolde gözlemleyin */
 
-
-
+function cumleKur2(kelime1, kelime2) {
+	console.log(kelime1 + " " + kelime2)
+}
+cumleKur2("Hello", "World!");
 
 /* (Oto test var) cumleKur fonksiyonuna 5 parametre göndererek "Ben iyi bir yazılımcı olacağım!" stringini elde edin, sonucu `bircumle` değişkenine atayın ve konsolde gözlemleyin */
 var bircumle;
 
-/* kodlar buraya */
-
+function cumleKur3(bir, iki, uc, dort, bes) {
+	return bir + " " + iki + " " + uc + " " + dort + " " + bes;
+}
+bircumle = cumleKur3("Ben", "iyi", "bir", "yazılımcı", "olacağım!");
+console.log(bircumle);
 
 
 //		Sayfanın en üstünde global olarak tanımlanmış `cumleler` adında bir dizi bulunmaktadır. Bu dizinin içinde en çok 5 en az 1 adet string bulunan diziler bulunmaktadır. Aşağıdaki görevlerde aksi belirtilmedikçe bu dizi kullanılacaktır.
@@ -102,12 +109,15 @@ var bircumle;
 			4. Oluşturulan her cümle yeni bir dizi oluşturulup o dizinin içine aktarılacak. 
 			5. Oluşturulan yeni dizi döndürülecek.
 	*/
-	
 
-function cumlelereDonustur(/* kodlar buraya */ ){
-	/* kodlar buraya */
+
+function cumlelereDonustur(cumleler, ayrac = ",") {
+	return cumleler.map(function (cumle) {
+		return cumle.join(ayrac);
+	})
 }
-
+var yeniDizi = cumlelereDonustur(cumleler, " ");
+console.log(yeniDizi);
 
 
 /* GÖREV 2:
@@ -119,9 +129,17 @@ function cumlelereDonustur(/* kodlar buraya */ ){
 			5. Oluşturulan bu dizinin tek sayılı anahtarlarını(index) [1,3,5,7,9] kullanarak ilk 5 elemanı geriçağırım olarak çağırılan cumleKur dizisinin parametreleri olarak aktarılarak çağırılacak ÖRNEK: callback(dizi[1],dizi[2],dizi[3],dizi[5],dizi[7])
 			6. Oluşturulan paragraf döndürülecek
 	*/
-	
-function paragrafOlustur(/* kodlar buraya */ ){
-	/* kodlar buraya */ 
+
+function paragrafOlustur(cumleler, cumleKur, cumlelereDonustur) {
+	const cumleDizisi = cumlelereDonustur(cumleler, " ");
+	// Çağırılacak dizinin anahtarlarını belirle
+	const anahtarlar = [1, 3, 5, 7, 9];
+	// İlk 5 cümleyi oluşturmak için parametreleri hazırla
+	const cumleParametreleri = anahtarlar.map((anahtar) => cumleDizisi[anahtar]);
+	// Cümleleri birleştirerek paragraf oluştur
+	const paragraf = cumleKur(...cumleParametreleri);
+	// Paragrafı döndür
+	return paragraf;
 }
 
 
@@ -130,17 +148,21 @@ function paragrafOlustur(/* kodlar buraya */ ){
 			3a. meyveler dizisinin ilk ve son elemanlarını diziden çıkartın. (.pop ve .shift metodlarını kullanın)
  */
 //3a çözümü
-/* kodlar buraya */
+meyveler.pop();
+meyveler.shift();
 
 
 
 
 
- 
+
 /* 			3b.  Bir tavşan ve bir kirpi arkadaşlar sebzeler dizimizin peşine düştü. Tavşan => 🐇 , Kirpi=> 🦔 , Tavşanla kirpi sebzeleri ele geçirmek için bir plan kurdular. Tavşan diziye önden saldıracak, kirpi ise arkalarından dolaşacak. Varsayalım ki arkadaşların planları başarılı oldu. Tavşanı dizinin ilk elemanına 🐇, Kirpiyi dizinin son elemanına ekleyin 🦔 
 */
 //3b çözümü
-/* kodlar buraya */
+let tavşan = "🐇";
+let kirpi = "🦔";
+sebzeler.push(kirpi);
+sebzeler.unshift(tavşan);
 
 
 
@@ -151,12 +173,12 @@ function paragrafOlustur(/* kodlar buraya */ ){
 
 /* 			3c. manav isminde bir dizi oluşturun.`meyveler` dizisi ilk elemanlara, `sebzeler` dizisi son elemanlara denk gelecek şekilde, iki diziyi birleştirip sonucu manav dizisine aktarın. (.concat metodu)
 	*/
-	//3c çözümü
+//3c çözümü
 /* kodlar buraya */
 
 var manav;
 
-
+manav = meyveler.concat(sebzeler);
 
 
 
@@ -170,21 +192,24 @@ var manav;
 			4. elde edilen string döndürülecek
  */
 
-function emojileriDonustur(/* kodlar buraya */){
-/* kodlar buraya */
-
+function emojileriDonustur(msj, emojiler) {
+	let result = msj;
+	for (let emojiKey in emojiler) {
+		result = result.replaceAll(emojiKey.toLowerCase(), emojiler[emojiKey]).replaceAll(emojiKey.toUpperCase(), emojiler[emojiKey]);
+	}
+	return result;
 }
 
 
 
 /*  Bu satırın aşağısındaki kodları lütfen değiştirmeyin  */
-function sa(){
-    console.log('Kodlar çalışıyor');
-    return 'as';
+function sa() {
+	console.log('Kodlar çalışıyor');
+	return 'as';
 }
 sa();
 module.exports = {
-    sa,
+	sa,
 	cumleler,
 	cumleKur,
 	bircumle,
